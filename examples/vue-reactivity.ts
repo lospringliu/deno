8f95674ce202809a9be88ebb751def0000405361
Xinchun Liu <xcliu@ca.ibm.com>
2020-05-17 14:37:13 -0400

    add an example for vue reactivity

---
+++ examples/vue-reactivity.ts
@@ -0,0 +1,22 @@
+// deno run https://raw.githubusercontent.com/lospringliu/deno/master/examples/vue-reactivity.ts
+
+import * as reactivity from "./vue/reactivity/index.ts"
+
+let total
+const price = reactivity.ref(10)
+const quantity = reactivity.ref(10)
+
+const effect = reactivity.effect(() => {
+	console.log(`price = ${price.value}`)
+	console.log(`quantiy = ${quantity.value}`)
+	total = price.value * quantity.value
+	console.log(`total = ${price.value} * ${quantity.value} = ${total}`)
+})
+
+price.value = 20
+
+quantity.value = 20
+
+price.value = 1
+
+quantity.value = 1
