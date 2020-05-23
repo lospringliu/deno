ee79dd6bdc3891e12fdc1d33f052c955ef4473f0
Xinchun Liu <xcliu@ca.ibm.com>
2020-05-23 15:36:46 -0400

    loop for watch

--- example-watch.ts
+++ example-watch.ts
@@ -1,36 +1,41 @@
 // deno run https://raw.githubusercontent.com/lospringliu/deno/master/example-reactivity.ts
 
 import * as reactivity from "./vue/reactivity/index.ts"
-import * as watch from "./vue/index.ts"
-// import * as watch from "./vue/apiWatch.ts"
+import * as runtime from "./vue/index.ts"
+// import * as runtime from "./vue/apiWatch.ts"
 
 const sleep = (time: number = 1000) => new Promise(res => setTimeout(() => res(), time))
 const reactive = setup()
 function setup () {
 	// const price = reactivity.ref(10)
 	// const quantity = reactivity.ref(10)
-	const state: any = reactivity.reactive({price: 10,
+	const state: any = reactivity.reactive({
+		price: 10,
 		quantity: 10,
 		total: reactivity.computed(() => state.price * state.quantity)
 	})
 	
-	watch.watch( () => state.price,
-		() => {
-		console.log(`price = ${state.price}`)
-		console.log(`quantiy = ${state.quantity}`)
-		console.log(`total: ${state.total}`)
-	})
+	runtime.watch(
+		() => state.price,
+		(value, oldValue) => {
+			console.log(`price = ${state.price}, changed from ${oldValue} to ${value}`)
+			console.log(`total: ${state.total}`)
+			console.log("---------------------")
+		}
+	)
+	runtime.watch(
+		() => state.quantity,
+		(value, oldValue) => {
+			console.log(`quantity = ${state.quantity}, changed from ${oldValue} to ${value}`)
+			console.log(`total: ${state.total}`)
+			console.log("                     ---------------------")
+		}
+	)
 	
 	return {
 		state
 	}
 }
 
-reactive.state.price = 20
-await sleep(2000)
-reactive.state.price = 50
-await sleep(2000)
-reactive.state.price = 100 
-await sleep(2000)
-reactive.state.quantity = 20
+setInterval(() => { reactive.state.price += 10; reactive.state.quantity += 5 }, 1000)
 
