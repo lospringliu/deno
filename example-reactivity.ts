ed782aef10e1646d7e3f3151898fe0a21d355449
Xinchun Liu <xcliu@ca.ibm.com>
2020-05-23 15:59:09 -0400

    fmt

--- example-reactivity.ts
+++ example-reactivity.ts
@@ -1,86 +1,91 @@
 // deno run https://raw.githubusercontent.com/lospringliu/deno/master/example-reactivity.ts
 
 // import * as reactivity from "./vue/reactivity/index.ts"
-import * as reactivity from "./vue/index.ts"
+import * as reactivity from "./vue/index.ts";
 
-let total
-const price = reactivity.ref(10)
-const quantity = reactivity.ref(10)
+let total;
+const price = reactivity.ref(10);
+const quantity = reactivity.ref(10);
 
 const feffect = reactivity.effect(() => {
-	console.log(`price = ${price.value}`)
-	console.log(`quantiy = ${quantity.value}`)
-	total = price.value * quantity.value
-	console.log(`total = ${price.value} * ${quantity.value} = ${total}`)
-})
+  console.log(`price = ${price.value}`);
+  console.log(`quantiy = ${quantity.value}`);
+  total = price.value * quantity.value;
+  console.log(`total = ${price.value} * ${quantity.value} = ${total}`);
+});
 
-price.value = 20
+price.value = 20;
 
-quantity.value = 20
+quantity.value = 20;
 
-price.value = 1
+price.value = 1;
 
-quantity.value = 1
+quantity.value = 1;
 
-console.log("stopping effect")
-reactivity.stop(feffect)
+console.log("stopping effect");
+reactivity.stop(feffect);
 
-price.value = 10
-quantity.value = 10
+price.value = 10;
+quantity.value = 10;
 
-const computed = reactivity.computed(() => price.value * quantity.value)
+const computed = reactivity.computed(() => price.value * quantity.value);
 
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
 
-quantity.value = 20
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
-price.value = 20
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
-price.value = 1
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
-quantity.value = 1
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
+quantity.value = 20;
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
+price.value = 20;
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
+price.value = 1;
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
+quantity.value = 1;
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
 
-console.log("stopping computed effect")
-reactivity.stop(computed.effect)
-quantity.value = 10000
-console.log(`price = ${price.value}`)
-console.log(`quantiy = ${quantity.value}`)
-console.log(`computed total: ${computed.value}`)
+console.log("stopping computed effect");
+reactivity.stop(computed.effect);
+quantity.value = 10000;
+console.log(`price = ${price.value}`);
+console.log(`quantiy = ${quantity.value}`);
+console.log(`computed total: ${computed.value}`);
 
-const factors = reactivity.reactive({price: 10, quantity: 10})
-let reffect
-const rreffect	= reactivity.effect(() => reffect = factors.price * factors.quantity, {lazy: true, onTrigger: () => console.log("triggered")})
-console.log(`reactive total: ${reffect}`)
-rreffect()
-const non_reactive = factors.price * factors.quantity
-const computed_reactive = reactivity.computed(() => factors.price * factors.quantity)
-console.log(`price = ${factors.price}`)
-console.log(`quantiy = ${factors.quantity}`)
-console.log(`non reactive total: ${non_reactive}`)
-console.log(`computed reactive total: ${computed_reactive.value}`)
-console.log(`reactive total: ${reffect}`)
+const factors = reactivity.reactive({ price: 10, quantity: 10 });
+let reffect;
+const rreffect = reactivity.effect(
+  () => reffect = factors.price * factors.quantity,
+  { lazy: true, onTrigger: () => console.log("triggered") },
+);
+console.log(`reactive total: ${reffect}`);
+rreffect();
+const non_reactive = factors.price * factors.quantity;
+const computed_reactive = reactivity.computed(() =>
+  factors.price * factors.quantity
+);
+console.log(`price = ${factors.price}`);
+console.log(`quantiy = ${factors.quantity}`);
+console.log(`non reactive total: ${non_reactive}`);
+console.log(`computed reactive total: ${computed_reactive.value}`);
+console.log(`reactive total: ${reffect}`);
 
-factors.price = 20
-console.log(`price = ${factors.price}`)
-console.log(`quantiy = ${factors.quantity}`)
-console.log(`non reactive total: ${non_reactive}`)
-console.log(`computed reactive total: ${computed_reactive.value}`)
-console.log(`reactive total: ${reffect}`)
+factors.price = 20;
+console.log(`price = ${factors.price}`);
+console.log(`quantiy = ${factors.quantity}`);
+console.log(`non reactive total: ${non_reactive}`);
+console.log(`computed reactive total: ${computed_reactive.value}`);
+console.log(`reactive total: ${reffect}`);
 
-factors.quantity = 20
-console.log(`price = ${factors.price}`)
-console.log(`quantiy = ${factors.quantity}`)
-console.log(`non reactive total: ${non_reactive}`)
-console.log(`computed reactive total: ${computed_reactive.value}`)
-console.log(`reactive total: ${reffect}`)
+factors.quantity = 20;
+console.log(`price = ${factors.price}`);
+console.log(`quantiy = ${factors.quantity}`);
+console.log(`non reactive total: ${non_reactive}`);
+console.log(`computed reactive total: ${computed_reactive.value}`);
+console.log(`reactive total: ${reffect}`);
--- example-watch.ts
+++ example-watch.ts
@@ -1,41 +1,48 @@
 // deno run https://raw.githubusercontent.com/lospringliu/deno/master/example-reactivity.ts
 
-import * as reactivity from "./vue/reactivity/index.ts"
-import * as runtime from "./vue/index.ts"
+import * as reactivity from "./vue/reactivity/index.ts";
+import * as runtime from "./vue/index.ts";
 // import * as runtime from "./vue/apiWatch.ts"
 
-const sleep = (time: number = 1000) => new Promise(res => setTimeout(() => res(), time))
-const reactive = setup()
-function setup () {
-	// const price = reactivity.ref(10)
-	// const quantity = reactivity.ref(10)
-	const state: any = reactivity.reactive({
-		price: 10,
-		quantity: 10,
-		total: reactivity.computed(() => state.price * state.quantity)
-	})
-	
-	runtime.watch(
-		() => state.price,
-		(value, oldValue) => {
-			console.log(`price = ${state.price}, changed from ${oldValue} to ${value}`)
-			console.log(`total: ${state.total}`)
-			console.log("---------------------")
-		}
-	)
-	runtime.watch(
-		() => state.quantity,
-		(value, oldValue) => {
-			console.log(`quantity = ${state.quantity}, changed from ${oldValue} to ${value}`)
-			console.log(`total: ${state.total}`)
-			console.log("                     ---------------------")
-		}
-	)
-	
-	return {
-		state
-	}
+const sleep = (time: number = 1000) =>
+  new Promise((res) => setTimeout(() => res(), time));
+const reactive = setup();
+function setup() {
+  // const price = reactivity.ref(10)
+  // const quantity = reactivity.ref(10)
+  const state: any = reactivity.reactive({
+    price: 10,
+    quantity: 10,
+    total: reactivity.computed(() => state.price * state.quantity),
+  });
+
+  runtime.watch(
+    () => state.price,
+    (value, oldValue) => {
+      console.log(
+        `price = ${state.price}, changed from ${oldValue} to ${value}`,
+      );
+      console.log(`total: ${state.total}`);
+      console.log("---------------------");
+    },
+  );
+  runtime.watch(
+    () => state.quantity,
+    (value, oldValue) => {
+      console.log(
+        `quantity = ${state.quantity}, changed from ${oldValue} to ${value}`,
+      );
+      console.log(`total: ${state.total}`);
+      console.log("                     ---------------------");
+    },
+  );
+
+  return {
+    state,
+  };
 }
 
-setInterval(() => { reactive.state.price += 10; reactive.state.quantity += 5 }, 1000)
-
+setInterval(() => {
+  reactive.state.price += 10;
+  reactive.state.quantity += 5;
+}, 1000);
